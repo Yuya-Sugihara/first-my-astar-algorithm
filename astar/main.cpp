bfd48ce8a4906591836338cc4b03245a197cea1c
yuuyamix <yuuyamix3@gmail.com>
2019-01-23 17:49:22 +0900

    Initial Commit

---
+++ astar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  astar
+//
+//  Created by 杉原悠哉 on 2019/01/23.
+//  Copyright © 2019 杉原悠哉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
